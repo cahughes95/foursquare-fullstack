4190609d5af9885691e003e1632b02ab6e268f77
Jeff Katzy <jeffreyerickatz@gmail.com>
2024-03-13 15:12:16 -0400

    update docker aws

--- foursquare/api/restaurants/__init__.py
+++ foursquare/api/restaurants/__init__.py
@@ -1,14 +1,15 @@
-from flask import Flask, jsonify
 import os
+
+from flask import Flask, jsonify
 from postgres import Postgres
 
 app = Flask(__name__)
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
-connection = Postgres(DATABASE_URL)
-
 
 @app.route('/locations')
 def locations():
-    locations = connection.all('SELECT * FROM locations;')
+    locations = [{
+        'id': 1, 'name': 'los tacos numero uno dos tres'
+    }]
     return jsonify({'locations': locations})
